26a0444114042a764f9e91c31450f83837de7964
David Wickes <dave.wickes@gmail.com>
2017-05-30 12:58:36 +0100

    removed content type check

--- index.js
+++ index.js
@@ -10,18 +10,12 @@ const linkInserter = createLinkInserterService()
 app.use(bodyParser.text({ type: 'text/html' }))
 
 app.post('/api/insert-cache-links', function (req, res, next) {
-  var contype = req.headers['content-type'];
-  if (!contype || contype.indexOf('text/html') !== 0 || !req.body)
-  {
-    return res.send(400)
-  }
-  
   createLinkInserterService().insertCacheLinks(req.body).then(function (parsedHtml) {
     console.log('parsedHtml', parsedHtml)
     if (!parsedHtml) {
       return res.send(400)
     }
-    res.send(parsedHtml)  
+    res.send(parsedHtml)
     next()
   })
 })
@@ -30,16 +24,16 @@ app.listen(3001, function () {
   console.log('listening on port 3001')
 })
 
-function createLinkInserterService() {
+function createLinkInserterService () {
   return linkInserterService({
-    cacheLinkGenerator(originalLink) {
-      // todo get from config 
+    cacheLinkGenerator (originalLink) {
+      // todo get from config
       const linkGeneratorRootUrl = 'http://localhost:4747'
-      const url = linkGeneratorRootUrl + '/save/' + originalLink 
-      return fetch(url, {method: 'HEAD'}).then(function(res) {
+      const url = linkGeneratorRootUrl + '/save/' + originalLink
+      return fetch(url, {method: 'HEAD'}).then(function (res) {
         return res.headers.get('Content-Location')
       })
-      // return 'cached/' + originalLink 
+      // return 'cached/' + originalLink
     }
   })
 }
